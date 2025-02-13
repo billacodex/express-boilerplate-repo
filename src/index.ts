0a8f1fdb7f27c55ce353953ab9986bc7afd42706
debilla <dimuthu.billa@gmail.com>
2025-02-13 14:20:33 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import "reflect-metadata";
 import cors from "cors";
-import sampleModuleRouter from "./modules/sample-module/sample-module.route";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(sampleModuleRouter);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
