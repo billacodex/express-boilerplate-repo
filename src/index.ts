e14aa2629079d51e28edcdde66d077b2f5d91a8a
debilla <dimuthu.billa@gmail.com>
2025-02-13 14:38:51 +0800

    chnages

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,10 @@ import express from "express";
 import "reflect-metadata";
 import cors from "cors";
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
