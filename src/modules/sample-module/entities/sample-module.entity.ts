0d3499243e712f7f2483b8fa45ada8da32e1ad26
deBilla <dimuthu.billa@gmail.com>
2025-07-09 15:34:25 +0800

    changes

--- src/modules/sample-module/entities/sample-module.entity.ts
+++ src/modules/sample-module/entities/sample-module.entity.ts
@@ -1,11 +1,12 @@
-import {IEntity} from "../../../core/entity/entity.interface";
 import {v4 as uuidv4} from "uuid";
+import { BaseEntity } from "../../../core/models/response/base.entity";
 
-export class SampleModuleEntity implements IEntity {
-  uuid!: string;
-  fullName!: string;
+export class SampleModuleEntity extends BaseEntity<SampleModuleEntity> {
+  uuid: string;
+  fullName: string;
 
   constructor(data: Partial<SampleModuleEntity> = {}) {
+    super(data);
     this.uuid = data.uuid || uuidv4();
     this.fullName = data.fullName || "";
   }
